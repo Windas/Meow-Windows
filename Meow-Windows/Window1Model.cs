b1a86ada3b10cf2ecbb23e47e8d9b54466ec8102
kalenty <kalenty@outlook.com>
2015-06-03 15:25:41 +0800

    gui更改

---
+++ Meow-Windows/Window1Model.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using Dragablz;
+using Dragablz.Dockablz;
+//using M.Annotations;
+
+namespace Meow_Windows
+{
+    public class Window1Model
+    {
+        private readonly IInterTabClient _interTabClient;
+        private readonly ObservableCollection<TabContent> _tabContents = new ObservableCollection<TabContent>();
+
+        public Window1Model()
+        {
+            _interTabClient = new DefaultInterTabClient();
+            
+            
+        }
+
+        public static Window1Model CreateWithSamples()
+        {
+            var result = new Window1Model();
+            
+            result.TabContents.Add(new TabContent("File 1", new EditArea()));
+
+            return result;
+        }
+
+        public ObservableCollection<TabContent> TabContents
+        {
+            get { return _tabContents; }
+        }
+
+        public IInterTabClient InterTabClient
+        {
+            get { return _interTabClient; }
+        }
+
+        public Func<object> NewItemFactory
+        {
+            get { return () => new TabContent("New", new EditArea()); }
+        }
+
+    }
+}
---
+++ Meow-Windows/Window1NewItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Dragablz;
+
+
+namespace Meow_Windows
+{
+    public static class Window1NewItem
+    {
+        static int num = 1;
+        public static Func<HeaderedItemViewModel> Factory
+        {
+            get
+            {
+                return
+                    () =>
+                    {
+                        return new HeaderedItemViewModel()
+                        {
+                            Header = "New File " + num++.ToString(),
+                            Content = new EditArea()
+                        };
+                    };
+            }
+        }
+    }
+}
